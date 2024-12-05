160f55aad51f87e24d731f1c91891604c6501701
Tsi2216 <getanehshimelis40@gmail.com>
2024-12-05 04:22:12 -0800

    Added functionality for users to add detailed descriptions to recipes

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from 'react';
+import { useRecipeStore } from './stores/recipeStore';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails'; 
+import EditRecipeForm from './components/EditRecipeForm';
 import './App.css';
 
 function App() {
+  // Access the Zustand store
+  const recipes = useRecipeStore(state => state.recipes);
+  const addRecipe = useRecipeStore(state => state.addRecipe);
+
   return (
-    <div>
-      <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <Router>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <AddRecipeForm addRecipe={addRecipe} />
+        <Routes>
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+          <Route path="/" element={<RecipeList recipes={recipes} />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useRecipeStore } from '../recipeStore';
+import { useRecipeStore } from '../stores/recipeStore';
 
 const RecipeList = () => {
   const recipes = useRecipeStore(state => state.recipes);
