7110d7aa8022fc960d0965aa2669045e3b9e027e
Tsi2216 <getanehshimelis40@gmail.com>
2024-12-12 04:50:34 -0800

    added  tailwind-react-integration/src/components/UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,8 +1,12 @@
 function UserProfile({ name, title, imageUrl, description }) {
     return (
-      <div className="user-profile bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-        <img className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto" src={imageUrl} alt="User" />
-        <h1 className="text-lg md:text-xl text-blue-800 my-4">{name}</h1>
+      <div className="user-profile bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
+        <img 
+          className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out" 
+          src={imageUrl} 
+          alt="User" 
+        />
+        <h1 className="text-lg md:text-xl text-blue-800 my-4 hover:text-blue-500">{name}</h1>
         <p className="text-sm md:text-base text-gray-600">{title}</p>
         <p className="text-sm md:text-base text-gray-600">
           {description ? description : "No description available."}
