8caf3904bdcc646e930017de600dc7cafb9dbbfb
Tsi2216 <getanehshimelis40@gmail.com>
2024-12-12 04:32:00 -0800

    added modified tailwind-react-integration/tailwind.config.js

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+  purge: [
     "./src/**/*.{js,jsx,ts,tsx}", // Include all JS and TS files in the src directory
     "./public/index.html" // Include the main HTML file
   ],
+  darkMode: 'media', // Enable dark mode based on user's system preference
+  variants: {
+    extend: {}, // Extend variants for utilities
+  },
   theme: {
     extend: {},
   },
