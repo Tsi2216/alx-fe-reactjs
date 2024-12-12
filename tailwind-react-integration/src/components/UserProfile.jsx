f946ad689205c3c10af3dd47bb3b9579bae16d62
Tsi2216 <getanehshimelis40@gmail.com>
2024-12-12 04:48:36 -0800

    added  tailwind-react-integration/src/components/UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 function UserProfile({ name, title, imageUrl, description }) {
     return (
       <div className="user-profile bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-        <img className="rounded-full w-24 h-24 md:w-36 md:h-36 mx-auto" src={imageUrl} alt="User" />
+        <img className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto" src={imageUrl} alt="User" />
         <h1 className="text-lg md:text-xl text-blue-800 my-4">{name}</h1>
         <p className="text-sm md:text-base text-gray-600">{title}</p>
         <p className="text-sm md:text-base text-gray-600">
