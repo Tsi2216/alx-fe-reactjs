2623a70b69b429f0bac556f8e4c4d690fb497057
Tsi2216 <getanehshimelis40@gmail.com>
2024-12-12 04:29:12 -0800

    added tailwind-react-integration

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all JS and TS files in the src directory
+    "./public/index.html" // Include the main HTML file
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
