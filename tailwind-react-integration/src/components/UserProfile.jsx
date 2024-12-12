f072ce74850585305a4e944e2d10ad50d81188a4
Tsi2216 <getanehshimelis40@gmail.com>
2024-12-12 04:43:58 -0800

    added modified tailwind-react-integration/src/components/UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,10 +1,10 @@
 function UserProfile({ name, title, imageUrl, description }) {
     return (
-      <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:scale-105 transition-transform duration-200">
-        <img className="rounded-full w-36 h-36 mx-auto" src={imageUrl} alt="User" />
-        <h1 className="text-xl text-blue-800 my-4">{name}</h1>
-        <p className="text-gray-600 text-base">{title}</p>
-        <p className="text-gray-600 text-base">
+      <div className="user-profile bg-gray-100 p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+        <img className="rounded-full w-24 h-24 md:w-36 md:h-36 mx-auto" src={imageUrl} alt="User" />
+        <h1 className="text-lg md:text-xl text-blue-800 my-4">{name}</h1>
+        <p className="text-sm md:text-base text-gray-600">{title}</p>
+        <p className="text-sm md:text-base text-gray-600">
           {description ? description : "No description available."}
         </p>
       </div>
