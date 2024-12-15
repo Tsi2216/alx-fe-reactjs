88bed7edbd95bef2a72cdf380cf96f735e32b127
Tsi2216 <getanehshimelis40@gmail.com>
2024-12-15 13:16:39 -0800

    added modified Search.jsx

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -20,7 +20,7 @@ const Search = () => {
             setUserData(data);
         } catch (err) {
             console.error(err); // Log the error for debugging
-            setError("Looks like we can’t find the user"); // Set the specific error message as a string
+            setError("Looks like we cant find the user"); // Set the specific error message as a string
         } finally {
             setLoading(false);
         }
