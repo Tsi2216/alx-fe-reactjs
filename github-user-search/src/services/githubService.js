e97e4be2666bdb2c0f810b194323d8dee657b668
Tsi2216 <getanehshimelis40@gmail.com>
2024-12-15 12:41:32 -0800

    added modified githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -4,3 +4,14 @@ export const fetchUserData = async (username) => {
     const response = await axios.get(`https://api.github.com/users/${username}`);
     return response.data;
 };
+
+// Handle 404 errors
+axios.interceptors.response.use(
+    response => response,
+    error => {
+        if (error.response && error.response.status === 404) {
+            throw new Error('User not found');
+        }
+        throw error;
+    }
+);
